4ce80312cb42b7cff5e5b635799c38dbe41bf67d
ScriptlessFoe <33396697+ScriptlessFoe@users.noreply.github.com>
2026-02-28 22:28:02 -0500

    detection for every tamplate added

--- src/main.py
+++ src/main.py
@@ -13,11 +13,23 @@ import os
 # - translate symbols into romanization
 # - (opt.) translate romanization back into clean symbols to double check.
 
-output_dir = "./output/"
-debug_dir = "./debug/"
+# clean output
+output_dir = "./output"
+for filename in os.listdir(output_dir):
+    file_path = os.path.join(output_dir, filename)
+    try:
+        # Check if the item is a file or a symbolic link, then delete
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        # If it's a directory, delete it and all its contents recursively
+        elif os.path.isdir(file_path):
+            import shutil
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print(f'Error deleting {file_path}: {e}')
 
 # import template and input images
-template_dir = "./templates/"
+template_dir = "./templates"
 template_names = sorted([f[:-4] for f in os.listdir(template_dir) if os.path.isfile(os.path.join(template_dir, f))])
 templates = sorted([os.path.join(template_dir, f) for f in os.listdir(template_dir) if os.path.isfile(os.path.join(template_dir, f))])
 templates_img = [cv.imread(name, cv.IMREAD_GRAYSCALE) for name in templates]
@@ -31,35 +43,22 @@ for template in templates_img:
     n_h = int(h*aspect_ratio)
     resized_templates_img.append(cv.resize(template, (n_w, n_h)))
 
-input_dir = "./input/"
+input_dir = "./input"
 input_names = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))] 
 inputs = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))] 
 inputs_img_rgb = [cv.imread(name) for name in inputs]
 inputs_img = [cv.imread(name, cv.IMREAD_GRAYSCALE) for name in inputs]
 
-#turn images into canny edges
-# canny_templates = []
-# for template in resized_templates_img:
-#     canny_templates.append(cv.Canny(template,100,200))
-
-# canny_inputs = []
-# for input in inputs_img:
-#     canny_inputs.append(cv.Canny(input,100,200))
-
-#     plt.subplot(121),plt.imshow(input,cmap = 'gray')
-#     plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-#     plt.subplot(122),plt.imshow(cv.Canny(input,100,200),cmap = 'gray')
-#     plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-    
-#     plt.show()
-
 # do template matching
 for i, input_img in enumerate(inputs_img):
+    scale_successes = {}
+    symbol_locs = []
+
     for j, template in enumerate(resized_templates_img):
         tw, th = template.shape[::-1]
-        iw, ih = input_img.shape[::-1]
+        # iw, ih = input_img.shape[::-1]
 
-        # loop over the scales of the image
+        # # loop over the scales of the image
         # for scale in np.linspace(0.2, 1.0, 20)[::-1]:
         #     # resize image
         #     resized_input = cv.resize(input_img, (int(iw*scale), int(ih*scale)))
@@ -68,13 +67,30 @@ for i, input_img in enumerate(inputs_img):
         #         break
 
         res = cv.matchTemplate(input_img, template, cv.TM_CCOEFF_NORMED)
-        threshold = 0.6
-        loc = np.where( res >= threshold)
-        img_cpy = inputs_img_rgb[i].copy()
-        for pt in zip(*loc[::-1]):
-            cv.rectangle(img_cpy, pt, (pt[0] + tw, pt[1] + th), (0,0,255), 2)
-        
-        cv.imwrite(os.path.join(output_dir, template_names[j]+"_"+input_names[i]), img_cpy)
+        threshold = 0.8
+        loc = np.where(res >= threshold)
+        if (len(loc[0]) != 0):
+            # record successful template matches
+
+            # if str(scale) in scale_successes:
+            #     scale_successes[str(scale)] += 1
+            # else:
+            #     scale_successes[str(scale)] = 1
+            
+            # draw box around identified symbols
+            for pt in zip(*loc[::-1]):
+                cv.rectangle(inputs_img_rgb[i], pt, (pt[0] + tw, pt[1] + th), (0,0,255), 2)
+
+        symbol_locs.append(loc)
+    
+    # print image of all detected symbols
+    cv.imwrite(os.path.join(output_dir, input_names[i]), inputs_img_rgb[i])
+
+    print(symbol_locs)
+
+
+
+
         
         
         
