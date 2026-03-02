2e4c1ebf9037b9a34fff119386df1b39a43b634f
ScriptlessFoe <33396697+ScriptlessFoe@users.noreply.github.com>
2026-03-01 19:03:03 -0500

    refactor

--- src/main.py
+++ src/main.py
@@ -15,40 +15,22 @@ import os
 
 DIST = 5
 GAP = 25
-OUTPUT_DIR = "./output"
 TEMPLATE_DIR = "./templates"
-INPUT_DIR = "./input"
-
-# clean output directory
-for filename in os.listdir(OUTPUT_DIR):
-    file_path = os.path.join(OUTPUT_DIR, filename)
-    try:
-        # Check if the item is a file or a symbolic link, then delete
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        # If it's a directory, delete it and all its contents recursively
-        elif os.path.isdir(file_path):
-            import shutil
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print(f'Error deleting {file_path}: {e}')
 
 # import template images
 template_names = sorted([f[:-4] for f in os.listdir(TEMPLATE_DIR) if os.path.isfile(os.path.join(TEMPLATE_DIR, f))])
 templates = sorted([os.path.join(TEMPLATE_DIR, f) for f in os.listdir(TEMPLATE_DIR) if os.path.isfile(os.path.join(TEMPLATE_DIR, f))])
 templates_img = [cv.imread(name, cv.IMREAD_GRAYSCALE) for name in templates]
 
-# import input images
-input_names = [f[:-4] for f in os.listdir(INPUT_DIR) if os.path.isfile(os.path.join(INPUT_DIR, f))] 
-inputs = [os.path.join(INPUT_DIR, f) for f in os.listdir(INPUT_DIR) if os.path.isfile(os.path.join(INPUT_DIR, f))] 
-inputs_img_rgb = [cv.imread(name) for name in inputs]
-inputs_img = [cv.imread(name, cv.IMREAD_GRAYSCALE) for name in inputs]
+# ----------------------------------------------------------------------------------------------------------------------------
+# main function def
+# ----------------------------------------------------------------------------------------------------------------------------
+def read_frumic(input_img_rgb):
+    input_img = cv.cvtColor(input_img_rgb, cv.COLOR_BGR2GRAY)
 
-# do template matching, with resizing
-for i, input_img in enumerate(inputs_img):
+    # do template matching, with resizing
     scale_successes = {}
     scale_symbol_locs = {}
-
     for j, template in enumerate(templates_img):
         tw, th = template.shape[::-1]
         iw, ih = input_img.shape[::-1]
@@ -94,14 +76,12 @@ for i, input_img in enumerate(inputs_img):
     #     print(scale_max)
     
     # make image of with boxes on all detected symbols
-    iw, ih = inputs_img_rgb[i].shape[1::-1]
-    inputs_img_rgb[i] = cv.resize(inputs_img_rgb[i], (int(iw*scale_max), int(ih*scale_max)))
+    iw, ih = input_img_rgb.shape[1::-1]
+    input_img_rgb = cv.resize(input_img_rgb, (int(iw*scale_max), int(ih*scale_max)))
     for loc in symbol_locs:
         for pt in zip(*loc[::-1]):
-            cv.rectangle(inputs_img_rgb[i], pt, (pt[0] + 15, pt[1] + 20), (0,0,255), 2)
-    write_status = cv.imwrite(os.path.join(OUTPUT_DIR, input_names[i] + ".jpg"), inputs_img_rgb[i])
-    if not write_status:
-        print(f"Failed to write image")
+            cv.rectangle(input_img_rgb, pt, (pt[0] + 15, pt[1] + 20), (0,0,255), 2)
+    input_img_rgb = cv.resize(input_img_rgb, (iw, ih))
 
     # group points by y value to get lines
     lines = {}
@@ -152,36 +132,63 @@ for i, input_img in enumerate(inputs_img):
             last = pt
         lines[key] = clean_line
 
-    # print, adding spaces
+    # create string, adding spaces
+    full_str = ""
+    for key in lines:
+        line = lines[key]
+        line_str = ""
+        last = line[0]
+        for pt in line:
+            # smaller symbols get smaller gaps
+            gap = (GAP - 5) if (last[1] == template_names.index("s") or last[1] == template_names.index("c")) else GAP
+            if abs(last[0][0] - pt[0][0]) >= gap:
+                line_str += " "
+            last = pt
+            ch = template_names[pt[1]]
+            if len(ch) == 1:
+                line_str += ch.upper()
+            else:
+                line_str += "["+ch+"]"
+        full_str += line_str + "\n"
+        
+    return full_str, input_img_rgb
+
+
+# ----------------------------------------------------------------------------------------------------------------------------
+# input and output reading starts here
+# ----------------------------------------------------------------------------------------------------------------------------
+OUTPUT_DIR = "./output"
+INPUT_DIR = "./input"
+
+# clean output directory
+for filename in os.listdir(OUTPUT_DIR):
+    file_path = os.path.join(OUTPUT_DIR, filename)
+    try:
+        # Check if the item is a file or a symbolic link, then delete
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        # If it's a directory, delete it and all its contents recursively
+        elif os.path.isdir(file_path):
+            import shutil
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print(f'Error deleting {file_path}: {e}')
+
+# import input images
+input_names = [f[:-4] for f in os.listdir(INPUT_DIR) if os.path.isfile(os.path.join(INPUT_DIR, f))] 
+inputs = [os.path.join(INPUT_DIR, f) for f in os.listdir(INPUT_DIR) if os.path.isfile(os.path.join(INPUT_DIR, f))] 
+inputs_img_rgb = [cv.imread(name) for name in inputs]
+
+# perform function on each input image
+for i, input_img in enumerate(inputs_img_rgb):
+    text, box_img = read_frumic(input_img)
+
     with open(os.path.join(OUTPUT_DIR, input_names[i] + ".txt"), "w") as f:
-        for key in lines:
-            line = lines[key]
-            line_str = ""
-            last = line[0]
-            for pt in line:
-                # smaller symbols get smaller gaps
-                gap = (GAP - 5) if (last[1] == template_names.index("s") or last[1] == template_names.index("c")) else GAP
-                if abs(last[0][0] - pt[0][0]) >= gap:
-                    line_str += " "
-                last = pt
-                ch = template_names[pt[1]]
-                if len(ch) == 1:
-                    line_str += ch.upper()
-                else:
-                    line_str += "["+ch+"]"
-            f.write(line_str)
-            f.write("\n")
-        f.close()
+        f.write(text)
+        f.close
     
+    write_status = cv.imwrite(os.path.join(OUTPUT_DIR, input_names[i] + ".jpg"), box_img)
+    if not write_status:
+        print(f"Failed to write image")
+
     
-            
-            
-
-
-
-
-
-
-        
-        
-        
